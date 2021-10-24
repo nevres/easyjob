c2fb303bcf31986aba388e5f5bb17c9488ee5bd8
Jahic Nevres <njahic@dccsintra.net>
2021-10-24 17:34:16 +0200

    Add Serilog logging to postgresql

--- backend/EasyJob/Program.cs
+++ backend/EasyJob/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NpgsqlTypes;
+using Serilog;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.PostgreSQL;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EasyJob
 {
@@ -13,14 +14,48 @@ namespace EasyJob
     {
         public static void Main(string[] args)
         {
+            RegisterLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void RegisterLogger()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{env}.json", optional: true)
+                .Build();
+
+            var connectionString = config.GetValue<string>("Serilog:ConnectionString");
+            var tableName = config.GetValue<string>("Serilog:TableName");
+            
+            //Used columns (Key is a column name) 
+            //Column type is writer's constructor parameter
+            IDictionary<string, ColumnWriterBase> columnWriters = new Dictionary<string, ColumnWriterBase>
+            {
+                {"message", new RenderedMessageColumnWriter(NpgsqlDbType.Text) },
+                {"message_template", new MessageTemplateColumnWriter(NpgsqlDbType.Text) },
+                {"level", new LevelColumnWriter(true, NpgsqlDbType.Varchar) },
+                {"raise_date", new TimestampColumnWriter(NpgsqlDbType.Timestamp) },
+                {"exception", new ExceptionColumnWriter(NpgsqlDbType.Text) },
+                {"properties", new LogEventSerializedColumnWriter(NpgsqlDbType.Jsonb) },
+                {"props_test", new PropertiesColumnWriter(NpgsqlDbType.Jsonb) },
+                {"machine_name", new SinglePropertyColumnWriter("MachineName", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") }
+            };
+
+            var logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.PostgreSQL(connectionString, tableName, columnWriters, needAutoCreateTable: true)
+                .CreateLogger();
+            Log.Logger = logger;
+        }
     }
 }
