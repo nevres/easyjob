182f5066671a8757fce6a0517422d40fe4cc5c0e
Jahic Nevres (DCCS) <njahic@dccsintra.net>
2021-11-24 21:57:16 +0100

    JobDocuements Models

---
+++ backend/Job.Domain/Entities/JobDocument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobProcessing.Domain.Entities
+{
+    public class JobDocument
+    {
+        public Guid DocumentId { get; private set; }
+        public string DocumentFileName { get; private set; }
+        public bool IsPrimary { get; private set; }
+        private JobDocument() { }
+
+        public JobDocument(Guid documentId, string documentFileName, bool isPrimary)
+        {
+            DocumentId = documentId;
+            DocumentFileName = documentFileName;
+            IsPrimary = isPrimary;
+        }
+    }
+}
